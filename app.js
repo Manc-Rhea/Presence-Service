493e1905b58a0792409386abe39ddbc60ec6ff1a
Manc-Rhea <61248800+Manc-Rhea@users.noreply.github.com>
2020-06-14 20:07:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyBb0hKL4Mh6qDNsf5a95R-G9UL28VR0ozY",
+    authDomain: "presence-app-6a31c.firebaseapp.com",
+    databaseURL: "https://presence-app-6a31c.firebaseio.com",
+    projectId: "presence-app-6a31c",
+    storageBucket: "presence-app-6a31c.appspot.com",
+    messagingSenderId: "834991016675",
+    appId: "1:834991016675:web:a73d24adbdbde2f8d3d3d6",
+    measurementId: "G-MNWNJNPJH9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
